8de41d9ab45a348c275ca9674e5fda7ce455d0e2
SPXcz <ondrachu@gmail.com>
2021-11-28 08:48:57 +0000

    Minor customization

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,9 +1,16 @@
 <ul>
-	<li><a href="#">First level entry</a></li>
 	<li>
-		<a href="#">First level container</a>
+		<a href="#"><h2>Fiter Options</h2></a>
 		<ul>
-			<li><a href="#">Second level entry</a></li>
+			<li><a href="#"> 
+				<label for="fileType">By file type:</label>
+				<select id="fileType" name="cars">
+				<option value="none">None</option>
+				<option value="image">Image</option>
+				<option value="text">Text</option>
+				<option value="diskImage">Disk Image</option>
+				</select> 
+			</a></li>
 			<li><a href="#">Second level entry</a></li>
 		</ul>
 	</li>
